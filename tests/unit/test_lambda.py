9f468599960323962f7c6202f023e542bd4c7273
German Gomez-Herrero <german@innovativetravel.eu>
2016-04-05 21:15:23 +0200

    Fix unit tests

--- tests/unit/test_lambda.py
+++ tests/unit/test_lambda.py
@@ -18,6 +18,8 @@ def test_process_event(kinesis_event, event, context, boto3_client,
                   partition_key)
     # The processor should have inserted the records in the output stream
     assert boto3_client("kinesis").put_records.call_count == 1
-    cal.assert_called_once_with(event, environment="environment",
-                                layer="layer", stage="stage",
-                                shard_id="shardId-000000000000")
+    state_args = dict(
+        environment="environment",
+        layer="layer", stage="stage",
+        shard_id="shardId-000000000000")
+    cal.assert_called_once_with(event, state_args=state_args)
