93ddfc26be5f3f0bf68313272c4546b743ab6b4b
German Gomez-Herrero <german@innovativetravel.eu>
2016-04-05 20:54:39 +0200

    Bundle together state arguments passed to user provided callables

--- humilis_kinesis_mapper/lambda_function/handler/processor/core.py
+++ humilis_kinesis_mapper/lambda_function/handler/processor/core.py
@@ -6,9 +6,10 @@
 def transform_events(events, callables, environment, layer, stage, shard_id):
     """Transforms events with the given callables."""
 
+    state_args = dict(environment=environment, layer=layer, stage=stage,
+                      shard_id=shard_id)
     for event in events:
         for cal in callables:
-            cal(event, environment=environment, layer=layer, stage=stage,
-                shard_id=shard_id)
+            cal(event, state_args=state_args)
 
     return events
--- tests/integration/mycode/myparserpkg/__init__.py
+++ tests/integration/mycode/myparserpkg/__init__.py
@@ -2,6 +2,7 @@
 
 import uuid
 
+import lambdautils.utils as utils
 from user_agents import parse
 
 
@@ -10,7 +11,12 @@ def partition_key(event, **kwargs):
     return event.get("client_id", str(uuid.uuid4()))
 
 
-def parse_ua(event, **kwargs):
+def parse_ua(event, state_args=None):
     parsed = parse(event["user_agent"])
     event["device_family"] = parsed.device.family or ""
     event["device_brand"] = parsed.device.brand or ""
+
+    utils.set_state("my_state_key", "my_state_value", namespace="my_namespace",
+                    **state_args)
+    assert utils.get_state("my_state_key", namespace="my_namespace",
+                           **state_args) == "my_state_value"
