3b076a7f682b8e0e418f76d319a6c53a4e3b052b
German Gomez-Herrero <german@innovativetravel.eu>
2016-07-25 20:46:13 +0200

    Upgrade lambdautils

--- humilis_kinesis_processor/lambda_function/setup.py
+++ humilis_kinesis_processor/lambda_function/setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=[
         "boto3",
         "raven",
-        "lambdautils>=0.6.6",
+        "lambdautils>=0.6.7",
         "werkzeug",
     ],
     classifiers=[
--- tests/integration/test_all.py
+++ tests/integration/test_all.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-"""Tests the input and output Kinesis streams."""
+"""Run integration tests."""
 
 import json
 import time
