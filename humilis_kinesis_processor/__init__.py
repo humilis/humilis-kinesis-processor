f90ee0d5d2b69e91824c22eba1e2971ff4fbeb34
German Gomez-Herrero <german@innovativetravel.eu>
2016-07-25 14:38:48 +0200

    Upgrade lambdautils

--- humilis_kinesis_processor/__init__.py
+++ humilis_kinesis_processor/__init__.py
@@ -1,3 +1,3 @@
 """Humilis kinesis processor."""
 
-__version__ = "0.6.8"
+__version__ = "0.6.9"
--- humilis_kinesis_processor/lambda_function/setup.py
+++ humilis_kinesis_processor/lambda_function/setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=[
         "boto3",
         "raven",
-        "lambdautils>=0.6.3",
+        "lambdautils>=0.6.4",
         "werkzeug",
     ],
     classifiers=[
