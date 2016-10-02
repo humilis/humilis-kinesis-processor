2e13632e2a43199f8d8890ccfa15f89f9515d4ce
German Gomez-Herrero <german@innovativetravel.eu>
2016-10-02 18:05:15 +0200

    Fix empty-bucket script

--- scripts/empty-bucket.py
+++ scripts/empty-bucket.py
@@ -8,6 +8,10 @@ import yaml
 
 def empty_bucket(outputs_file):
     """Empty the bucket associated to the test deployment."""
+
+    if not os.path.isfile(outputs_file):
+        print("Environment outputs not found: cannot empty bucket")
+        return
     with open(outputs_file, "r") as f:
         outputs = yaml.load(f)
 
